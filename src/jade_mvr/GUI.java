190063cbbfe8dcc31cc67df83b9b07d9660c83e0
Miguel Vila <miviro0804@gmail.com>
2024-11-19 11:58:08 +0100

    changes

--- src/jade_mvr/GUI.java
+++ src/jade_mvr/GUI.java
@@ -1,8 +1,14 @@
 package src.jade_mvr;
 
 import javax.swing.*;
+import javax.swing.text.NumberFormatter;
+
+import src.jade_mvr.MainAgent.GameParametersStruct;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.text.DecimalFormat;
 
 public class GUI extends JFrame {
     private JMenuBar menuBar;
@@ -13,6 +19,14 @@ public class GUI extends JFrame {
     private JPanel rightPanel;
     private JPanel statsPanel;
     private JPanel logPanel;
+    private JButton newGameButton;
+    private JButton stopButton;
+    private JButton continueButton;
+    private JButton playAllRoundsButton;
+    private JButton playXRoundsButton;
+    private JSpinner playXRoundsSpinner;
+    private JLabel playXRoundsLabel;
+    private JTextArea logTextArea;
 
     public GUI() {
         setTitle("JADE MVR");
@@ -20,11 +34,11 @@ public class GUI extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
+        createLogPanel();
         createMenuBar();
         createActionsPanel();
         createConfigPanel();
         createRightPanel();
-        createLogPanel();
     }
 
     private void createMenuBar() {
@@ -40,14 +54,77 @@ public class GUI extends JFrame {
         actionsPanel = new JPanel();
         actionsPanel.setBorder(BorderFactory.createTitledBorder("Actions"));
         actionsPanel.setPreferredSize(new Dimension(800, 60));
+
+        newGameButton = new JButton("New Game");
+        stopButton = new JButton("Stop");
+        continueButton = new JButton("Continue");
+        playAllRoundsButton = new JButton("Play All Rounds");
+
+        playXRoundsButton = new JButton("Play");
+        playXRoundsSpinner = new JSpinner(new SpinnerNumberModel(1, 1, 1000, 1));
+        playXRoundsLabel = new JLabel("rounds:");
+
+        actionsPanel.add(newGameButton);
+        actionsPanel.add(stopButton);
+        actionsPanel.add(continueButton);
+        actionsPanel.add(playAllRoundsButton);
+        actionsPanel.add(playXRoundsButton);
+        actionsPanel.add(playXRoundsSpinner);
+        actionsPanel.add(playXRoundsLabel);
+
         add(actionsPanel, BorderLayout.NORTH);
     }
 
     private void createConfigPanel() {
-        configPanel = new JPanel();
+        configPanel = new JPanel(new BorderLayout());
         configPanel.setBorder(BorderFactory.createTitledBorder("Configuration"));
         configPanel.setPreferredSize(new Dimension(240, 540));
+
+        JPanel bottomConfigPanel = new JPanel();
+        bottomConfigPanel.setBorder(BorderFactory.createTitledBorder("Bottom Configuration"));
+        bottomConfigPanel.setPreferredSize(new Dimension(240, 270));
+
+        configPanel.add(createParametersPanel(), BorderLayout.NORTH);
+        configPanel.add(bottomConfigPanel, BorderLayout.SOUTH);
+
         add(configPanel, BorderLayout.WEST);
+    }
+
+    private JPanel createParametersPanel() {
+        JPanel parametersPanel = new JPanel(new GridLayout(4, 2, 10, 10));
+        // Retrieve current parameters
+        GameParametersStruct params = MainAgent.getGameParameters();
+        // Create number-only formatted text fields with default values
+        JFormattedTextField nField = new JFormattedTextField(new NumberFormatter(new DecimalFormat("#")));
+        nField.setValue(params.N);
+        JFormattedTextField sField = new JFormattedTextField(new NumberFormatter(new DecimalFormat("#")));
+        sField.setValue(params.S);
+        JFormattedTextField rField = new JFormattedTextField(new NumberFormatter(new DecimalFormat("#")));
+        rField.setValue(params.R);
+        JFormattedTextField iField = new JFormattedTextField(new NumberFormatter(new DecimalFormat("#")));
+        iField.setValue(params.I);
+        parametersPanel.add(new JLabel("Number of players (N):"));
+        parametersPanel.add(nField);
+        parametersPanel.add(new JLabel("Stock exchange fee (S%):"));
+        parametersPanel.add(sField);
+        parametersPanel.add(new JLabel("Number of rounds (R):"));
+        parametersPanel.add(rField);
+        parametersPanel.add(new JLabel("Inflation rate (I%):"));
+        parametersPanel.add(iField);
+
+            try {
+                int n = ((Number) nField.getValue()).intValue();
+                int s = ((Number) sField.getValue()).intValue();
+                int r = ((Number) rField.getValue()).intValue();
+                int i = ((Number) iField.getValue()).intValue();
+                MainAgent.setGameParameters(new GameParametersStruct(n, s, r, i));
+                appendLog("Parameters set to: N=" + n + ", S=" + s + ", R=" + r + ", I=" + i);
+            } catch (Exception e) {
+                appendLog("Invalid input for parameters.");
+            }
+        
+
+        return parametersPanel;
     }
 
     private void createRightPanel() {
@@ -65,6 +142,17 @@ public class GUI extends JFrame {
         logPanel = new JPanel();
         logPanel.setBorder(BorderFactory.createTitledBorder("Log"));
         logPanel.setPreferredSize(new Dimension(800, 140));
+        logTextArea = new JTextArea();
+        logTextArea.setEditable(false);
+        logTextArea.setLineWrap(true);
+        logTextArea.setWrapStyleWord(true);
+        JScrollPane scrollPane = new JScrollPane(logTextArea);
+        scrollPane.setPreferredSize(new Dimension(780, 120));
+        logPanel.add(scrollPane);
         add(logPanel, BorderLayout.SOUTH);
+    }
+
+    public void appendLog(String text) {
+        logTextArea.append(text + "\n");
     }
 }
--- src/jade_mvr/MainAgent.java
+++ src/jade_mvr/MainAgent.java
@@ -1,22 +1,59 @@
 package src.jade_mvr;
 
+import jade.core.Agent;
+
 public class MainAgent {
-    private int count;
-    private GUI view;
+    private static GUI view;
+    private static GameParametersStruct gameParameters;
 
-    public MainAgent() {
-        this.count = 0; // Initialize the counter
-        this.view = new GUI();
+    public static GameParametersStruct getGameParameters() {
+        return gameParameters;
+    }
 
+    public static void setGameParameters(GameParametersStruct gameParameters) {
+        MainAgent.gameParameters = gameParameters;
+    }
+
+    public static void main(String[] args) {
+        gameParameters = new GameParametersStruct();
+        view = new GUI();
         // Show the GUI
         view.setVisible(true);
+        view.appendLog("Application started");
+        view.appendLog("Application started1");
+        view.appendLog("Application started2");
     }
 
 
     private void updateView() {
     }
 
-    public static void main(String[] args) {
-        new MainAgent(); // Start the application
+    public static class GameParametersStruct {
+        int N;
+        int R;
+        int S;
+        int I;
+
+        public GameParametersStruct() { // TODO: set default R=500, I=1
+            N = 2;
+            R = 50;
+            S = 4;
+            I = 0;
+        }
+
+        public GameParametersStruct(int n, int r, int s, int i) {
+            N = n;
+            R = r;
+            S = s;
+            I = i;
+        }
+
+        @Override
+        public String toString() {
+            return "N=" + N +
+            ", R=" + R +
+                    ", S=" + S +
+                    ", I=" + I;
+        }
     }
 }
